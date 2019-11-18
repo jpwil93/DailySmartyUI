575aefa4bd5ee143b114cc46574d85096bd9172e
SoapyPigeons <jpwil93@icloud.com>
2019-11-18 14:37:55 -0700

    created recent posts and search bar componenets and used thme in the home component

---
+++ src/components/searchBar.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class SearchBar extends Component {
+  render() {
+    return (
+      <form>
+        <input placeholder="Search DailySmarty"></input>
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
