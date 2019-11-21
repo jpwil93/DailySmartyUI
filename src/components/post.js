3e45b019cb34431d692c8055d1b722678f6cac23
SoapyPigeons <jpwil93@icloud.com>
2019-11-21 11:23:54 -0700

    modified post component to render results post and setup render links

--- src/components/post.js
+++ src/components/post.js
@@ -12,13 +12,39 @@ class Post extends Component {
     return topics;
   }
 
+  renderLinks() {
+    let links = this.props.post_links.map((post_link, index) => {
+      return (
+        <div className="post-link" key={index}>
+          <div className="post-link-box"></div>
+
+          <div className="post-link-link">
+            <a href={post_link.link_url}>Useful Link #{index + 1}</a>
+          </div>
+        </div>
+      );
+    });
+    return links;
+  }
+
   render() {
-    return (
-      <li className="recent-post">
-        <div className="recent-post__title">{this.props.title}</div>
-        <div className="recent-post__topics">{this.renderTopics()}</div>
-      </li>
-    );
+    if (this.props.type == "recent") {
+      return (
+        <li className="recent-post">
+          <div className="recent-post__title">{this.props.title}</div>
+          <div className="recent-post__topics">{this.renderTopics()}</div>
+        </li>
+      );
+    } else if (this.props.type == "result") {
+      return (
+        <li className="result-post">
+          <div className="result-post-topics">{this.renderTopics()}</div>
+          <div className="result-post-title">{this.props.title}</div>
+
+          <div className="result-post-links">{this.renderLinks()}</div>
+        </li>
+      );
+    }
   }
 }
 
