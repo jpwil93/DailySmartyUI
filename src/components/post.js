826715875b5008c9bdad5351f291a8a29ae81c84
SoapyPigeons <jpwil93@icloud.com>
2019-11-22 11:01:16 -0700

    added styles for results and result-post components

--- src/components/post.js
+++ src/components/post.js
@@ -4,10 +4,12 @@ import AnimateHeight from "react-animate-height";
 class Post extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
       height: 0
     };
   }
+
   renderTopics() {
     let topics = this.props.associated_topics.map((topic, index) => {
       return (
@@ -23,9 +25,8 @@ class Post extends Component {
     let links = this.props.post_links.map((post_link, index) => {
       return (
         <div className="post-link" key={index}>
-          <div className="post-link-box"></div>
-
-          <div className="post-link-link">
+          <div className="post-link__box"></div>
+          <div className="post-link__link">
             <a href={post_link.link_url}>Useful Link #{index + 1}</a>
           </div>
         </div>
@@ -38,15 +39,17 @@ class Post extends Component {
     if (this.props.type == "recent") {
       return (
         <li className="recent-post">
-          <div className="recent-post__title">{this.props.title}</div>
+          <div className="recent-post__title">
+            <a href={this.props.url_for_post}>{this.props.title}</a>
+          </div>
           <div className="recent-post__topics">{this.renderTopics()}</div>
         </li>
       );
     } else if (this.props.type == "result") {
       return (
         <li className="result-post">
-          <div className="result-post-topics">{this.renderTopics()}</div>
-          <div className="result-post-title">
+          <div className="result-post__topics">{this.renderTopics()}</div>
+          <div className="result-post__title">
             <a
               href={this.props.url_for_post}
               onMouseEnter={() => this.setState({ height: 70 })}
@@ -56,7 +59,7 @@ class Post extends Component {
             </a>
           </div>
           <AnimateHeight duration={500} height={this.state.height}>
-            <div className="result-post-links">{this.renderLinks()}</div>
+            <div className="result-post__links">{this.renderLinks()}</div>
           </AnimateHeight>
         </li>
       );
